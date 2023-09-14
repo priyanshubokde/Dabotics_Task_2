8838894bd2faaed925b4ad28457996d08211661d
Priyanshu Bokde <114168740+priyanshubokde@users.noreply.github.com>
2023-09-14 18:29:29 +0530

    Add files via upload

---
+++ scripy.js
@@ -0,0 +1,29 @@
+let displayValue = "";
+
+function appendToDisplay(value) {
+  displayValue += value;
+  document.getElementById("display").value = displayValue;
+}
+
+function clearDisplay() {
+  displayValue = "";
+  document.getElementById("display").value = displayValue;
+}
+
+function deleteLastDigit() {
+  displayValue = displayValue.slice(0, -1);
+  document.getElementById("display").value = displayValue;
+}
+
+function performOperation(operator) {
+  appendToDisplay(" " + operator + " ");
+}
+
+function calculateResult() {
+  try {
+    displayValue = eval(displayValue);
+    document.getElementById("display").value = displayValue;
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
+}
